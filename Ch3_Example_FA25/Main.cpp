23dfc57b6cfa436bb41bc0937266b098aafb91ec
BuschMath <busch@iowacentral.edu>
2025-09-15 09:25:58 -0500

    Add project files.

---
+++ Ch3_Example_FA25/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	int x = 0;
+	if (x = 0)
+	{
+		std::cout << "True" << std::endl;
+	}
+	else
+	{
+		std::cout << "False" << std::endl;
+	}
+
+	std::cout << "Other output" << std::endl;
+
+	return 0;
+}
